477d20fcc5df74fbaea45c874f81222d6325b471
Matthew Uribe <matt.mee19@gmail.com>
2018-07-05 14:45:50 -0700

    Bouncing Balls

--- Week_2/Processing JS/sketch.js
+++ Week_2/Processing JS/sketch.js
@@ -1,4 +1,4 @@
-var circle = {
+/*var circle = {
     diameter: 80,
     xCoor: 0, 
     yCoor: 0, 
@@ -6,40 +6,57 @@ var circle = {
     xSpeed: 15,
     ySpeed: 15 
 }
+*/
+
 
 function randomColor(){
     return [random(289), random(289), random(289)];
 }
 
-function setup() {
-    createCanvas(1300, 680);
-    background(234, 425, 134);
-    frameRate(15);
+
+
+
+function Circle(x,y, size, color, xSpeed, ySpeed){
+    this.xCoor = x;
+    this.yCoor = y;
+    this.diameter = size;
+    this.color = color || [0,0,0]; 
+    this.xSpeed = xSpeed || 5;
+    this.ySpeed = ySpeed || 7;
 }
 
+var circleList= [];
+
+function createCircle(){
+    var numCircles = random ([2, 5, 9, 16]);
+    for (var i = 0; i < numCircles; i++){
+        circleList.push(new Circle(random(width), random(height), 50));
+    }
+}
+
+function setup() {
+    createCanvas(parseInt(prompt("Enter a value for the width")),parseInt(prompt("Enter a value for the height")));
+    createCircle();
+}
+
+
 function draw() {
-    fill(circle.color);
-    ellipse(circle.xCoor, circle.yCoor, circle.diameter);
-    //console.log("The x coordinate is" + circle.xCoor);
-    if(circle.xCoor > 1300){
-        circle.color = randomColor();
-        circle.xSpeed = -circle.xSpeed;
-    }else if(circle.xCoor < 0){
-        circle.color = randomColor();
-        circle.xSpeed = -circle.xSpeed;
+    background(27, 74, 150);
+    for(var i = 0; i < circleList.length; i++){
+        fill(circleList[i].color);
+        ellipse(circleList[i].xCoor, circleList[i].yCoor, circleList[i].diameter);
+        if(circleList[i].xCoor > width || circleList[i].xCoor < 0){
+            circleList[i].color = randomColor();
+            circleList[i].xSpeed = -circleList[i].xSpeed;
+        }
+
+        if(circleList[i].yCoor > height || circleList[i].yCoor < 0){
+            circleList[i].ySpeed = -circleList[i].ySpeed;
+            circleList[i].diameter = random(20, 90);
+
+        }
+
+        circleList[i].xCoor += circleList[i].xSpeed;
+        circleList[i].yCoor += circleList[i].ySpeed;
     }
-    
-    if(circle.yCoor > 680){
-        circle.ySpeed = -circle.ySpeed;
-        circle.diameter = random(20, 90);
-        
-    }else if(circle.yCoor < 0){
-        circle.ySpeed = -circle.ySpeed;
-        circle.color = randomColor();
-        circle.diameter = 50;
-    }
-     console.log("The y coordinate is" + circle.yCoor);
-    
-    circle.xCoor += circle.xSpeed;
-    circle.yCoor += circle.ySpeed;
 }
